8971899de09eeff0dd56ed962af4637c044b9465
youngsterjaidev <jaidevv999@gmail.com>
2026-02-22 17:15:27 +0530

    feat: Add `shouldShowBanner` and `shouldShowList` notification settings.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,6 +29,8 @@ Notifications.setNotificationHandler({
     shouldShowAlert: true,
     shouldPlaySound: true,
     shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
   }),
 });
 
